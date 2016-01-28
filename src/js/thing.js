1591310bf9e810f98104f8a07da0e73cdc9a7651
Christopher Groskopf <chrisgroskopf@gmail.com>
2016-01-28 13:19:49 -0600

    Resize on interval.

--- src/js/thing.js
+++ src/js/thing.js
@@ -1,38 +1,14 @@
 var fm = require('./fm');
 var throttle = require('./throttle');
-var features = require('./detectFeatures')();
-
-function init () {
-// initialize the thing, load data, parse info, etc
-}
-
-function setup () {
-// setup the thing, insert DOM elements, bind data, etc
-
-	//now that everything is setup, update
-	update()
-}
-
-function update () {
-// update the thing, position, size, and style DOM elemements
-	
-	// adjust iframe for dynamic content
-	fm.resize()
-}
 
 function resize() {
-// on resize, update save dimensional values and update.
-	
-	update()
-	fm.resize()
+	fm.resize();
 }
 
-var throttleRender = throttle(resize, 250);
+var throttleResize = throttle(resize, 1000);
 
 $(document).ready(function () {
-	// adjust iframe for loaded content
-	fm.resize()
-	$(window).resize(throttleRender);
-	init();
-	setup();
+	resize()
+	$(window).resize(throttleResize);
+	window.setInterval(throttleResize, 1000);
 });
